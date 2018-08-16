b81e12e9cc9bc4f7e3fcd2622df684dfc36c9fe7
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-08-16 15:34:44 +0200

    Logs commented out

--- mcumgr-android-lib/src/main/java/io/runtime/mcumgr/ble/McuMgrBleTransport.java
+++ mcumgr-android-lib/src/main/java/io/runtime/mcumgr/ble/McuMgrBleTransport.java
@@ -274,10 +274,10 @@ public class McuMgrBleTransport extends BleManager<BleManagerCallbacks> implemen
         disconnect().enqueue();
     }
 
-    @Override
-    public void log(int level, @NonNull String message) {
-        Log.d(TAG, message);
-    }
+//    @Override
+//    public void log(int level, @NonNull String message) {
+//        Log.d(TAG, message);
+//    }
 //
 //    @Override
 //    public void log(int level, int messageRes, Object... params) {
