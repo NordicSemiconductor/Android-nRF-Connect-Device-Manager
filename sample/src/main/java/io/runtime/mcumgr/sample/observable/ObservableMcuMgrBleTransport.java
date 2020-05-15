57d72c533b5b7c17a73b4ae7cdc67225642b2136
Brian Giori <briangiori@gmail.com>
2020-05-15 14:45:06 -0700

    add license header to new files from #71

--- sample/src/main/java/io/runtime/mcumgr/sample/observable/ObservableMcuMgrBleTransport.java
+++ sample/src/main/java/io/runtime/mcumgr/sample/observable/ObservableMcuMgrBleTransport.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.runtime.mcumgr.sample.observable;
 
 import android.bluetooth.BluetoothDevice;
