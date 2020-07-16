947aba87f3702a1c9e8d3c4446337dc37bf8eea0
Brian Giori <briangiori@gmail.com>
2020-07-16 11:11:59 -0700

    make payloads internal

--- serialization/src/main/kotlin/Payload.kt
+++ serialization/src/main/kotlin/Payload.kt
@@ -4,54 +4,54 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties("operation", "group", "command")
-abstract class Payload
+internal sealed class Payload
 
-data class EchoRequestPayload(
+internal data class EchoRequestPayload(
     @JsonProperty("d") val echo: String
 ) : Payload()
 
-data class EchoResponsePayload(
+internal data class EchoResponsePayload(
     @JsonProperty("r") val echo: String
 ) : Payload()
 
-data class ImageWriteRequestPayload(
+internal data class ImageWriteRequestPayload(
     @JsonProperty("data") val data: ByteArray,
     @JsonProperty("off") val offset: Int,
     @JsonProperty("len") val size: Int? = null,
     @JsonProperty("sha") val hash: ByteArray? = null
 ) : Payload()
 
-data class ImageWriteResponsePayload(
+internal data class ImageWriteResponsePayload(
     @JsonProperty("off") val offset: Int
 ) : Payload()
 
-data class CoreReadRequestPayload(
+internal data class CoreReadRequestPayload(
     @JsonProperty("off") val offset: Int
 ) : Payload()
 
-data class CoreReadResponsePayload(
+internal data class CoreReadResponsePayload(
     @JsonProperty("data") val data: ByteArray,
     @JsonProperty("off") val offset: Int,
     @JsonProperty("len") val length: Int? = null
 ) : Payload()
 
-data class FileWriteRequestPayload(
+internal data class FileWriteRequestPayload(
     @JsonProperty("name") val fileName: String,
     @JsonProperty("data") val data: ByteArray,
     @JsonProperty("off") val offset: Int,
     @JsonProperty("len") val length: Int? = null
 ) : Payload()
 
-data class FileWriteResponsePayload(
+internal data class FileWriteResponsePayload(
     @JsonProperty("off") val offset: Int
 ) : Payload()
 
-data class FileReadRequestPayload(
+internal data class FileReadRequestPayload(
     @JsonProperty("name") val fileName: String,
     @JsonProperty("off") val offset: Int
 ) : Payload()
 
-data class FileReadResponsePayload(
+internal data class FileReadResponsePayload(
     @JsonProperty("data") val data: ByteArray,
     @JsonProperty("off") val offset: Int,
     @JsonProperty("len") val length: Int? = null
