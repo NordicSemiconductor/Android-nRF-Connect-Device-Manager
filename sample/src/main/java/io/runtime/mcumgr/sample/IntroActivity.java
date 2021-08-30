b47b61385a521407d6706293496852c213cfbcbd
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-08-30 10:17:22 +0200

    ButterKnife removed from IntroActivity

--- sample/src/main/java/io/runtime/mcumgr/sample/IntroActivity.java
+++ sample/src/main/java/io/runtime/mcumgr/sample/IntroActivity.java
@@ -1,42 +1,35 @@
 package io.runtime.mcumgr.sample;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.net.URL;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import io.runtime.mcumgr.sample.databinding.ActivityIntroBinding;
 
 public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        final ActivityIntroBinding binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        findViewById(R.id.button_continue).setOnClickListener(v -> finish());
+        binding.buttonContinue.setOnClickListener(v -> finish());
 
-        findViewById(R.id.intro_link_nrf_connect_sdk).setOnClickListener(v ->
+        binding.introLinkNrfConnectSdk.setOnClickListener(v ->
                 open(Uri.parse("https://www.nordicsemi.com/Software-and-Tools/Software/nRF-Connect-SDK"))
         );
-        findViewById(R.id.intro_link_smp_sample).setOnClickListener(v ->
+        binding.introLinkSmpSample.setOnClickListener(v ->
                 open(Uri.parse("https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/zephyr/samples/subsys/mgmt/mcumgr/smp_svr/README.html"))
         );
-        findViewById(R.id.intro_link_source_code).setOnClickListener(v ->
+        binding.introLinkSourceCode.setOnClickListener(v ->
                 open(Uri.parse("https://github.com/NordicSemiconductor/Android-nRF-Connect-Device-Manager"))
-        );
-        findViewById(R.id.intro_link_mcu_manager).setOnClickListener(v ->
-                open(Uri.parse("https://github.com/JuulLabs-OSS/mcumgr-android"))
         );
 
         try {
@@ -46,7 +39,6 @@ public class IntroActivity extends AppCompatActivity {
         } catch (final PackageManager.NameNotFoundException e) {
             // Ignore
         }
-
     }
 
     private void open(@NonNull final Uri uri) {
