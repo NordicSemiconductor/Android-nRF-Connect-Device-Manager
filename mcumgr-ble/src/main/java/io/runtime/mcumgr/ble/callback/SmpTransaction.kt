50d08751b2bab637919a27e5858cc4562e527b2f
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-02-09 15:29:14 +0100

    Improvement: Improved exception presentation

--- mcumgr-ble/src/main/java/io/runtime/mcumgr/ble/callback/SmpTransaction.kt
+++ mcumgr-ble/src/main/java/io/runtime/mcumgr/ble/callback/SmpTransaction.kt
@@ -4,11 +4,21 @@ import java.util.concurrent.TimeoutException
 
 class TransactionTimeoutException internal constructor(
     val id: Int
-) : TimeoutException("Transaction $id timed out without receiving a response")
+) : TimeoutException("Transaction $id timed out without receiving a response") {
+
+    override fun toString(): String {
+        return message!!
+    }
+}
 
 class TransactionOverwriteException internal constructor(
     val id: Int
-) : Exception("Transaction $id has been overwritten")
+) : Exception("Transaction $id has been overwritten") {
+
+    override fun toString(): String {
+        return message!!
+    }
+}
 
 internal interface SmpTransaction {
     fun send(data: ByteArray)
