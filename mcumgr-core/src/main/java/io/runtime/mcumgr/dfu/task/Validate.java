b6db3e02ab7b75ad5b35fe781c2b974c141494bc
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-10-20 18:39:18 +0200

    Support for DirectXip without Revert

--- mcumgr-core/src/main/java/io/runtime/mcumgr/dfu/task/Validate.java
+++ mcumgr-core/src/main/java/io/runtime/mcumgr/dfu/task/Validate.java
@@ -15,7 +15,9 @@ import io.runtime.mcumgr.dfu.model.McuMgrTargetImage;
 import io.runtime.mcumgr.exception.McuMgrErrorException;
 import io.runtime.mcumgr.exception.McuMgrException;
 import io.runtime.mcumgr.image.McuMgrImage;
+import io.runtime.mcumgr.managers.DefaultManager;
 import io.runtime.mcumgr.managers.ImageManager;
+import io.runtime.mcumgr.response.dflt.McuMgrBootloaderInfoResponse;
 import io.runtime.mcumgr.response.img.McuMgrImageStateResponse;
 import io.runtime.mcumgr.task.TaskManager;
 
@@ -51,7 +53,52 @@ class Validate extends FirmwareUpgradeTask {
 	@Override
 	public void start(@NotNull final TaskManager<Settings, State> performer) {
 		final Settings settings = performer.getSettings();
+		final DefaultManager manager = new DefaultManager(settings.transport);
+
+		// Starting from NCS 2.5 different bootloader modes allow sending the image in
+		// slightly different ways. For that, we need to read bootloader info.
+		// If that command is not supported, we assume the old, normal way of sending.
+		manager.bootloaderInfo(DefaultManager.BOOTLOADER_INFO_QUERY_BOOTLOADER, new McuMgrCallback<>() {
+			@Override
+			public void onResponse(@NotNull final McuMgrBootloaderInfoResponse response) {
+				LOG.trace("Bootloader name: {}", response.bootloader);
+
+				if ("MCUboot".equals(response.bootloader)) {
+					manager.bootloaderInfo(DefaultManager.BOOTLOADER_INFO_MCUBOOT_QUERY_MODE, new McuMgrCallback<>() {
+						@Override
+						public void onResponse(@NotNull McuMgrBootloaderInfoResponse response) {
+							LOG.info("Bootloader is in mode: {}, no downgrade: {}", parseMode(response.mode), response.noDowngrade);
+							validate(performer,
+									response.mode == McuMgrBootloaderInfoResponse.MODE_DIRECT_XIP || response.mode == McuMgrBootloaderInfoResponse.MODE_DIRECT_XIP_WITH_REVERT,
+									response.mode != McuMgrBootloaderInfoResponse.MODE_DIRECT_XIP);
+						}
+
+						@Override
+						public void onError(@NotNull McuMgrException error) {
+							// Pretend nothing happened.
+							validate(performer, false, true);
+						}
+					});
+				} else {
+					// It's some unknown bootloader. Try sending the old way.
+					validate(performer, false, true);
+				}
+			}
+
+			@Override
+			public void onError(@NotNull final McuMgrException error) {
+				// Pretend nothing happened.
+				validate(performer, false, true);
+			}
+		});
+	}
+
+	private void validate(@NotNull final TaskManager<Settings, State> performer,
+						  final boolean noSwap,
+						  final boolean allowRevert) {
+		final Settings settings = performer.getSettings();
 		final ImageManager manager = new ImageManager(settings.transport);
+
 		manager.list(new McuMgrCallback<>() {
 			@Override
 			public void onResponse(@NotNull final McuMgrImageStateResponse response) {
@@ -86,7 +133,7 @@ class Validate extends FirmwareUpgradeTask {
 
 					// The following flags will be updated based on the received slot information.
 					boolean found = false;     // An image with the same hash was found on the device
-					boolean skip = false;	   // When this flag is set the image will not be uploaded
+					boolean skip = false;      // When this flag is set the image will not be uploaded
 					boolean pending = false;   // TEST command was sent
 					boolean permanent = false; // CONFIRM command was sent
 					boolean confirmed = false; // Image has booted and confirmed itself
@@ -105,10 +152,10 @@ class Validate extends FirmwareUpgradeTask {
 							confirmed = slot.confirmed;
 							active = slot.active;
 
-							// If the image has been found on the secondary slot and it's confirmed,
+							// If the image has been found on its target slot and it's confirmed,
 							// we just need to restart the device in order for it to be swapped back to
 							// primary slot.
-							if (confirmed && slot.slot == image.slot) {
+							if (confirmed && slot.slot == image.slot && !noSwap) {
 								resetRequired = true;
 							}
 							break;
@@ -155,57 +202,62 @@ class Validate extends FirmwareUpgradeTask {
 					}
 					if (!found) {
 						performer.enqueue(new Upload(mcuMgrImage, imageIndex));
+						if (!allowRevert || mode == Mode.NONE) {
+							resetRequired = true;
+						}
 					}
-					switch (mode) {
-						case TEST_AND_CONFIRM -> {
-							// If the image is not pending (test command has not been sent) and not
-							// confirmed (another image is under test), and isn't the currently
-							// running image, send test command and update the flag.
-							if (!pending && !confirmed && !active) {
-								performer.enqueue(new Test(mcuMgrImage.getHash()));
-								pending = true;
+					if (allowRevert && mode != Mode.NONE) {
+						switch (mode) {
+							case TEST_AND_CONFIRM -> {
+								// If the image is not pending (test command has not been sent) and not
+								// confirmed (another image is under test), and isn't the currently
+								// running image, send test command and update the flag.
+								if (!pending && !confirmed && !active) {
+									performer.enqueue(new Test(mcuMgrImage.getHash()));
+									pending = true;
+								}
+								// If the image is pending, reset is required.
+								if (pending) {
+									resetRequired = true;
+								}
+								if (!permanent && !confirmed) {
+									performer.enqueue(new ConfirmAfterReset(mcuMgrImage.getHash()));
+								}
 							}
-							// If the image is pending, reset is required.
-							if (pending) {
-								resetRequired = true;
+							case TEST_ONLY -> {
+								// If the image is not pending (test command has not been sent) and not
+								// confirmed (another image is under test), and isn't the currently
+								// running image, send test command and update the flag.
+								if (!pending && !confirmed && !active) {
+									performer.enqueue(new Test(mcuMgrImage.getHash()));
+									pending = true;
+								}
+								// If the image is pending, reset is required.
+								if (pending) {
+									resetRequired = true;
+								}
 							}
-							if (!permanent && !confirmed) {
-								performer.enqueue(new ConfirmAfterReset(mcuMgrImage.getHash()));
-							}
-						}
-						case TEST_ONLY -> {
-							// If the image is not pending (test command has not been sent) and not
-							// confirmed (another image is under test), and isn't the currently
-							// running image, send test command and update the flag.
-							if (!pending && !confirmed && !active) {
-								performer.enqueue(new Test(mcuMgrImage.getHash()));
-								pending = true;
-							}
-							// If the image is pending, reset is required.
-							if (pending) {
-								resetRequired = true;
-							}
-						}
-						case CONFIRM_ONLY -> {
-							// If the firmware is not confirmed yet, confirm t.
-							if (!permanent && !confirmed) {
-								performer.enqueue(new Confirm(mcuMgrImage.getHash()));
-								permanent = true;
-							}
-							if (permanent) {
-								resetRequired = true;
+							case CONFIRM_ONLY -> {
+								// If the firmware is not confirmed yet, confirm t.
+								if (!permanent && !confirmed) {
+									performer.enqueue(new Confirm(mcuMgrImage.getHash()));
+									permanent = true;
+								}
+								if (permanent) {
+									resetRequired = true;
+								}
 							}
 						}
 					}
 				}
 				// To make sure the reset command are added just once, they're added based on flags.
 				if (initialResetRequired) {
-					performer.enqueue(new ResetBeforeUpload());
+					performer.enqueue(new ResetBeforeUpload(noSwap));
 				}
 				if (resetRequired) {
 					if (eraseSettings)
 						performer.enqueue(new EraseStorage());
-					performer.enqueue(new Reset());
+					performer.enqueue(new Reset(noSwap));
 				}
 
 				performer.onTaskCompleted(Validate.this);
@@ -216,5 +268,18 @@ class Validate extends FirmwareUpgradeTask {
 				performer.onTaskFailed(Validate.this, e);
 			}
 		});
+	}
+
+	private String parseMode(final int mode) {
+		switch (mode) {
+			case 0: return "Single App";
+			case 1: return "Swap Scratch";
+			case 2: return "Overwrite-only";
+			case 3: return "Swap Without Scratch";
+			case 4: return "Direct XIP Without Revert";
+			case 5: return "Direct XIP With Revert";
+			case 6: return "RAM Loader";
+			default: return "Unknown (" + mode + ")";
+		}
 	}
 }
