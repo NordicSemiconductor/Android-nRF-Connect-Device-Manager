6105c6559c72a5b8feb9a7313b6518e0829d4924
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-08-07 12:23:16 +0200

    Bumping some log messages to higher level based on content

--- sample/src/main/java/io/runtime/mcumgr/sample/application/Dagger2Application.java
+++ sample/src/main/java/io/runtime/mcumgr/sample/application/Dagger2Application.java
@@ -7,9 +7,11 @@
 package io.runtime.mcumgr.sample.application;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.bluetooth.BluetoothDevice;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 import javax.inject.Inject;
 
@@ -21,6 +23,7 @@ import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasAndroidInjector;
 import io.runtime.mcumgr.sample.di.AppInjector;
 import io.runtime.mcumgr.sample.di.component.McuMgrSubComponent;
+import no.nordicsemi.android.log.LogContract;
 import no.nordicsemi.android.log.timber.nRFLoggerTree;
 import timber.log.Timber;
 
@@ -64,8 +67,36 @@ public class Dagger2Application extends Application implements HasAndroidInjecto
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
             deviceName = device.getName();
         }
-        final nRFLoggerTree logger = new nRFLoggerTree(this, device.getAddress(), deviceName); // name can be null, not an issue
+
+        // Add a custom logger tree that will log all messages from McuManager to nRF Logger app.
+        // The name can be null, it's not an issue.
+        final nRFLoggerTree logger = new nRFLoggerTree(this, device.getAddress(), deviceName) {
+
+            // By default, logs are mapped to the corresponding log levels.
+            // Here we override the log levels based on the message content, only to make the logs
+            // more readable in the nRF Logger app.
+            @SuppressLint("WrongConstant")
+            @Override
+            protected void log(int priority, final String tag, final String message, final Throwable t) {
+                if (getSession() == null)
+                    return;
+
+                int level = priority;
+                // Print messages starting with Sending and Received as APPLICATION level.
+                // Those contain parsed CBOR values and are printed in McuMgrBleTransport.
+                if (message.startsWith("Sending") || message.startsWith("Received")) {
+                    level = LogContract.Log.Level.APPLICATION;
+                }
+                // RC is returned in case of an error. "err" is returned in Version 2 of SMP protocol.
+                if (message.contains("\"rc\":") || message.contains("\"err\":")) {
+                    level = Log.WARN;
+                }
+                super.log(level, tag, message, t);
+            }
+
+        };
         Timber.plant(this.logger = logger);
+
         builder.target(device);
         if (logger.getSession() != null) {
             builder.logSessionUri(logger.getSession().getSessionUri());
