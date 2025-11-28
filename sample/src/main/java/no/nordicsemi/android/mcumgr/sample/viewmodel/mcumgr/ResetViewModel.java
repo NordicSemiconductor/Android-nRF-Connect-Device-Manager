0c90932adc2cb1fbe079636096290c06892aad4c
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2025-11-28 13:58:41 +0100

    Setting FL adv name in Image/Advanced

--- sample/src/main/java/no/nordicsemi/android/mcumgr/sample/viewmodel/mcumgr/ResetViewModel.java
+++ sample/src/main/java/no/nordicsemi/android/mcumgr/sample/viewmodel/mcumgr/ResetViewModel.java
@@ -7,8 +7,13 @@
 package no.nordicsemi.android.mcumgr.sample.viewmodel.mcumgr;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.StandardCharsets;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,18 +23,23 @@ import no.nordicsemi.android.mcumgr.McuMgrTransport;
 import no.nordicsemi.android.mcumgr.ble.exception.McuMgrDisconnectedException;
 import no.nordicsemi.android.mcumgr.exception.McuMgrException;
 import no.nordicsemi.android.mcumgr.managers.DefaultManager;
+import no.nordicsemi.android.mcumgr.managers.SettingsManager;
+import no.nordicsemi.android.mcumgr.response.McuMgrResponse;
 import no.nordicsemi.android.mcumgr.response.dflt.McuMgrOsResponse;
 
 public class ResetViewModel extends McuMgrViewModel {
     private final DefaultManager manager;
+    private final SettingsManager settings;
 
     private final MutableLiveData<McuMgrException> errorLiveData = new MutableLiveData<>();
 
     @Inject
     ResetViewModel(final DefaultManager manager,
+                   final SettingsManager settings,
                    @Named("busy") final MutableLiveData<Boolean> state) {
         super(state);
         this.manager = manager;
+        this.settings = settings;
     }
 
     @NonNull
@@ -37,8 +47,41 @@ public class ResetViewModel extends McuMgrViewModel {
         return errorLiveData;
     }
 
-    public void reset(final @DefaultManager.BootMode int bootMode, final boolean force) {
+    public void reset(final @DefaultManager.BootMode int bootMode, final boolean force, final @Nullable String advName) {
         setBusy();
+        if (advName != null && !advName.isEmpty()) {
+            settings.write("fw_loader/adv_name", advName.getBytes(StandardCharsets.UTF_8), new McuMgrCallback<>() {
+                @Override
+                public void onResponse(@NotNull McuMgrResponse response) {
+                    settings.save(new McuMgrCallback<>() {
+                        @Override
+                        public void onResponse(@NotNull McuMgrResponse response) {
+                            // Finally, with the name set, reset the device.
+                            reset(bootMode, force);
+                        }
+
+                        @Override
+                        public void onError(@NotNull McuMgrException error) {
+                            errorLiveData.postValue(error);
+                            postReady();
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(@NonNull final McuMgrException error) {
+                    // Seems like setting the name isn't supported.
+                    // Let the user know, don't pretend it worked.
+                    errorLiveData.postValue(error);
+                    postReady();
+                }
+            });
+        } else {
+            reset(bootMode, force);
+        }
+    }
+
+    private void reset(final @DefaultManager.BootMode int bootMode, final boolean force) {
         manager.reset(bootMode, force, new McuMgrCallback<>() {
             @Override
             public void onResponse(@NonNull final McuMgrOsResponse response) {
